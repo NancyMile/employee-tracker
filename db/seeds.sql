0ac11ee70f1f39c3df58e3de4ad9a073cb7114d2
nancy <smithnancy@outlook.com>
2022-08-22 23:59:17 +1000

    seed added

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,24 @@
+INSERT INTO departments (name)
+VALUES ("Sales"),
+       ("Engineering"),
+       ("Finance"),
+       ("Legal");
 
-       
+INSERT INTO roles (title, salary, department_id)
+VALUES ('Sales Person', 80000, 1),
+       ('Lead Engineer', 150000, 2),
+       ('Software Engineer', 120000, 2),
+       ('Account Manager', 160000, 3),
+       ('Accountant', 125000, 3),
+       ('Team Leader Legal', 250000, 4),
+       ('Lawyer', 190000, 4);
+
+INSERT INTO employees (first_name, last_name, manager_id, role_id)
+VALUES ('Mark','Antony',,1),
+       ('James','White',,2),
+       ('Nancy','Stanton',2,3),
+       ('Vimal','Kumar',,4),
+       ('Dilan','li',4,5),
+       ('Soyeb','kan',,6),
+       ('Mary','Brown',6,7),
+       ('Matt','Francis',2,3);
